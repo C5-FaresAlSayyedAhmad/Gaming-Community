12b1db4c6031332732f75018f5e63e5f80da53dc
FaresAlSayyedAhmad <fares.a.alsayyed@gmail.com>
2022-05-12 03:01:41 +0300

    edit usersRouter to have a nested router usersIdRouter

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,15 +1,28 @@
 const express = require("express");
+
+// Import users controller
 const { register , userInfo, userPosts ,userFavGames ,addFavGames ,userComments} = require("../controllers/users");
 
 
 const usersRouter = express.Router();
 
-usersRouter.post("/", register);
-usersRouter.get("/:id", userInfo);
-usersRouter.get("/:id/posts", userPosts);
-usersRouter.get("/:id/games ", userFavGames);
-usersRouter.post("/:id/games", addFavGames);
-usersRouter.get("/:id/comments ", userComments);
+const usersIdRouter  = express.Router();
 
+// post request the will create a new account for the user
+usersRouter.post("/", register);
+
+//usersRouter will use the usersIdRouter when the end point after /users will be /:userId
+usersRouter.use("/:userId" ,usersIdRouter)
+
+// get request that will get the a specifec user infromation
+usersIdRouter.get("/", userInfo);
+// get request that will get all the posts for the user
+usersIdRouter.get("/posts", userPosts);
+// get request that will get all the posts for the user
+usersIdRouter.get("/favgames ", userFavGames);
+// post request that will add favgames for the user 
+usersIdRouter.post("/favgames", addFavGames);
+// get request that will get all the comments for the user
+usersIdRouter.get("/comments ", userComments);
 
 module.exports = usersRouter;
