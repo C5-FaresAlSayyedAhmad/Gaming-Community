7d20332aa030f10f1cb4959f054e769cb1c2deee
FaresAlSayyedAhmad <fares.a.alsayyed@gmail.com>
2022-05-12 03:36:55 +0300

    edit gamesRouter that will use gameid router as a nested router and the gameid will use the postidRouter as a nested router

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -1,18 +1,36 @@
 const express = require("express");
 
 // Import games controller
-const { games ,addNewPost , allGamePosts , getPostById ,updatePost ,deletePost } = require("../controllers/games");
+const { getAllGames ,addNewPost , allGamePosts , getPostById ,updatePost ,deletePost } = require("../controllers/games");
 
 const gamesRouter = express.Router();
 
+const gamesIdRouter = express.Router();
 
-gamesRouter.get("/", games);
+const postIdRouter = express.Router();
 
-gamesRouter.post("/:gameId/", addNewPost);
-gamesRouter.get("/:gameId/ ", allGamePosts);
+// get request that will get all the games
+gamesRouter.get("/", getAllGames);
+gamesRouter.post("/", createGame);
 
-gamesRouter.get("/:gameId/:postId", getPostById);
-gamesRouter.put("/:gameId/:postId", updatePost);
-gamesRouter.delete("/:gameId/:postId", deletePost);
+
+//gamesRouter will use the gamesIdRouter when the end point after /games will be /:gamesUd
+gamesRouter.use("/:gameid", gamesIdRouter)
+
+// post request that will post a new post 
+gamesIdRouter.post("/", addNewPost);
+// post request that will get all the game posts
+gamesIdRouter.get("/", allGamePosts);
+
+
+//gamesIdRouter will use the postIdRouter when the end point after /:gamesId will be /:postId
+gamesIdRouter.use("/:postId", postIdRouter)
+
+// get request that will get a specifec post for a specifec game
+postIdRouter.get("/", getPostById);
+// put request that will update the post
+postIdRouter.put("/", updatePost);
+// delete request that will delete the post
+postIdRouter.delete("/", deletePost);
 
 module.exports = gamesRouter;
