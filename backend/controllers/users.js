e44f7e65c964ec873decc4c8fd2df5d8a75521f6
FaresAlSayyedAhmad <fares.a.alsayyed@gmail.com>
2022-05-12 03:43:14 +0300

    create register function

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,46 @@
 const usersModel = require("../models/users");
 
-const register = (req, res) => {};
+const register = (req, res) => {
+
+  const { firstName, lastName, userName, email, password } = req.body;
+  const user = new usersModel({
+    firstName,
+    lastName,
+    userName,
+    email,
+    password,
+  });
+
+  user
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: `Account Created Successfully`,
+        author: result,
+      });
+    })
+    .catch((err) => {
+      if (err.keyPattern.email == 1) {
+        return res.status(409).json({
+          success: false,
+          message: `The email already exists`,
+        });
+      }
+      else if (err.keyPattern.userName == 1) {
+        return res.status(409).json({
+          success: false,
+          message: `The username is used please try a different one`,
+        });
+      }
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
+    });
+
+};
 
 const userInfo = (req, res) => {};
 
