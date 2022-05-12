5c78b2f758caf59f5e4f9bc94f51b1f2f8eda126
FaresAlSayyedAhmad <fares.a.alsayyed@gmail.com>
2022-05-12 03:31:50 +0300

    login router

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -5,7 +5,7 @@ const  login  = require("../controllers/login");
 
 const loginRouter = express.Router();
 
-
+// post request the will login the user if he have registered 
 loginRouter.post("/", login);
 
 module.exports = loginRouter;
