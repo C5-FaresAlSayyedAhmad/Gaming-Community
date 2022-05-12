dc7fe1438655fb0e7363bb1cf1485dbbcc5b8c58
FaresAlSayyedAhmad <fares.a.alsayyed@gmail.com>
2022-05-12 12:14:53 +0300

    complete createCategory function and delete comment in login function

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -4,7 +4,31 @@ const getAllCategories = (req, res) => {};
 
 const getCategory = (req, res) => {};
 
-const createCategory = (req, res) => {};
+const createCategory = (req, res) => {
+  const { title, games } = req.body;
+  const category = new categoryModel({
+    title,
+    games,
+  });
 
+  category
+    .save()
+    .then((result) => {
+      res.status(201);
+      res.json({
+        success: true,
+        message: "New Category created",
+        category: result,
+      });
+    })
+    .catch((err) => {
+      res.status(500);
+      res.send({
+        success: false,
+        message: "Servcer Error",
+        err: err.message,
+      });
+    });
+};
 
-module.exports = { getAllCategories, getCategory ,createCategory };
+module.exports = { getAllCategories, getCategory, createCategory };
