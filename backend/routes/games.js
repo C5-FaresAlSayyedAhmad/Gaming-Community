f4a2b3a50a5e844a9397b5d248f1b3d36ce5c38f
FaresAlSayyedAhmad <fares.a.alsayyed@gmail.com>
2022-05-14 16:31:21 +0300

    use mergeParams to be able to use categoryid params

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -3,19 +3,19 @@ const express = require("express");
 // Import games controller
 const { getAllGames ,addNewPost , allGamePosts , getPostById ,updatePost ,deletePost ,createGame } = require("../controllers/games");
 
-const gamesRouter = express.Router();
+const gamesRouter = express.Router({mergeParams:true});
 
-const gamesIdRouter = express.Router();
+const gamesIdRouter = express.Router({mergeParams:true});
 
-const postIdRouter = express.Router();
+const postIdRouter = express.Router({mergeParams:true});
 
 // get request that will get all the games
 gamesRouter.get("/", getAllGames);
 gamesRouter.post("/", createGame);
 
-
 //gamesRouter will use the gamesIdRouter when the end point after /games will be /:gamesUd
 gamesRouter.use("/:gameid", gamesIdRouter)
+
 
 // post request that will post a new post 
 gamesIdRouter.post("/", addNewPost);
