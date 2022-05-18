88f9d7b7d26743fbb48f9cb83fe84b96928c60c4
FaresAlSayyedAhmad <fares.a.alsayyed@gmail.com>
2022-05-18 06:31:38 +0300

    finished  App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,38 @@
+import { Routes, Route } from "react-router-dom";
+import React, { useState, createContext } from "react";
 
 import './App.css';
+import Register from "./components/Register";
+import Login from "./components/Login";
+import NavBar from "./components/Navbar";
+import Games from "./components/AllGames";
+import Categories from "./components/AllCategories";
+import Category from "./components/Category";
+import Game from "./components/Game";
+import Post from "./components/Post";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      
+      <NavBar/>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Games />} />
+        <Route path="/category/" element={<Categories />} />
+        <Route path="/" element={<Games />} />
+        <Route path="/category/:categoryid/games" element={<Category />} />
+        <Route path="/category/:categoryid/games/:gameid"  element={<Game />} />
+        <Route path="/games/:gameid"  element={<Game />} />
+        <Route path="/category/:categoryid/games/:gameid/posts/:postid"  element={<Post />} />
+        <Route path="/games/:gameid/posts/:postid"  element={<Post />} />
+      </Routes>
+     
+      
+      
+
     </div>
   );
 }
